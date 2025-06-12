c7c017b683c1a9e3455ee0a4d8efe448f7ad4e8c
윤다빈 <116141225+czmcm5@users.noreply.github.com>
2025-06-13 00:58:46 +0900

    스토리북 재빌드

--- src/stories/Card.stories.ts
+++ src/stories/Card.stories.ts
@@ -19,14 +19,31 @@ export const Small: Story = {
   args: {
     size: 'small',
     color: 'mint',
+    MM: '02',
+    YY: '02',
+    name: '한도카드',
+    cardNum: '0101 0202 0303 4004',
   },
 };
 
-export const Medium: Story = {};
+export const Medium: Story = {
+  args: {
+    size: 'medium',
+    color: 'green',
+    MM: '02',
+    YY: '02',
+    name: '한도카드',
+    cardNum: '0101 0202 0303 4004',
+  },
+};
 
 export const Large: Story = {
   args: {
     size: 'large',
-    color: 'red',
+    color: 'organge',
+    MM: '02',
+    YY: '02',
+    name: '한도카드',
+    cardNum: '0101 0202 0303 4004',
   },
 };
